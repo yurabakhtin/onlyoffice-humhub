f763f9ebfc7c5649c1782c45beab7f760e075861
Andrew <andrew.panov@onlyoffice.com>
2021-07-21 16:13:56 +0300

    do not try to close editors/modal window after convertation

--- resources/humhub.onlyoffice.js
+++ resources/humhub.onlyoffice.js
@@ -142,15 +142,23 @@ humhub.module('onlyoffice', function (module, require, $) {
     function refreshFileInfo(that, evt) {
         client.post({url: that.options.fileInfoUrl}).then(function (response) {
             event.trigger('humhub:file:modified', [response.file]);
-            that.docEditor.destroyEditor();
-            that.modal.clear();
-            that.modal.close();
+            if (that.docEditor) {
+                that.docEditor.destroyEditor();
+            }
+            if (that.modal) {
+                that.modal.clear();
+                that.modal.close();
+            }
             evt.finish();
         }).catch(function (e) {
-            that.docEditor.destroyEditor();
+            if (that.docEditor) {
+                that.docEditor.destroyEditor();
+            }
             module.log.error(e);
-            that.modal.clear();
-            that.modal.close();
+            if (that.modal) {
+                that.modal.clear();
+                that.modal.close();
+            }
             evt.finish();
         });
     }
