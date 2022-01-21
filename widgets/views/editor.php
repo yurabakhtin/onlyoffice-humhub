35fd24b72577f6e6a23016d068d2fd975771bd4a
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2022-01-21 16:34:44 +0300

    buttons position on popup header

--- widgets/views/editor.php
+++ widgets/views/editor.php
@@ -19,8 +19,8 @@ if ($documentType === Module::DOCUMENT_TYPE_SPREADSHEET) {
 ?>
 
 <?= Html::beginTag('div', $options) ?>
-<div style = "height:50px; border-radius: 8px 8px 0px 0px; background-color:<?= $headerBackgroundColor; ?>; padding-top:6px; padding-right:12px">
-    <div class = "pull-right" style = "margin-top:8px;margin-right:12px">
+<div style = "height:50px; border-radius: 8px 8px 0px 0px; background-color:<?= $headerBackgroundColor; ?>; padding-top:7px; padding-right:7px">
+    <div class = "pull-right">
         <?php if ($mode === Module::OPEN_MODE_EDIT && !Yii::$app->user->isGuest): ?>
             <?= humhub\libs\Html::a(Yii::t('OnlyofficeModule.base', 'Share'), '#', ['class' => 'btn btn btn-default', 'data-action-click' => 'share', 'data-action-block' => 'sync', 'data-action-url' => Url::to(['/onlyoffice/share', 'guid' => $file->guid, 'mode' => $mode])]); ?>
         <?php endif; ?>
